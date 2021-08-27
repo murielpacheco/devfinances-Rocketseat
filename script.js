ee436b71776b2cb3fe69157c5949719b6adbfcaf
Muriel Damazio <murieldps@gmail.com>
2021-08-27 10:24:49 -0300

    devFinances - Rocketseat

---
+++ script.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector(".modal-overlay");
+const modalObj = {
+  toggleModal() {
+      //adicionar ou remove a classe active do modal
+      modal.classList.toggle("active");
+  }
+};
