100fed9b8395d4e48a36b9f9af3963139096e390
Muriel Damazio <murieldps@gmail.com>
2021-10-19 15:15:58 -0300

    Feature: Dark mode

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const modal = document.querySelector(".modal-overlay");
+const modal = document.querySelector('.modal-overlay');
 const modalObj = {
   toggleModal() {
     //adicionar ou remove a classe active do modal
-    modal.classList.toggle("active");
+    modal.classList.toggle('active');
   },
 };
 
 const Storage = {
   get() {
-    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
   },
 
   set(transactions) {
     localStorage.setItem(
-      "dev.finances:transactions",
+      'dev.finances:transactions',
       JSON.stringify(transactions)
     );
   },
@@ -60,10 +60,10 @@ const Transaction = {
 };
 
 const DOM = {
-  transactionsContainer: document.querySelector("#data-table tbody"),
+  transactionsContainer: document.querySelector('#data-table tbody'),
 
   addTransaction(transaction, index) {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
     tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
     tr.dataset.index = index;
 
@@ -71,7 +71,7 @@ const DOM = {
   },
 
   innerHTMLTransaction(transaction, index) {
-    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
 
     const amount = Utils.formatCurrency(transaction.amount);
 
@@ -88,44 +88,44 @@ const DOM = {
   },
 
   updateBalance() {
-    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(
       Transaction.incomes()
     );
-    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(
       Transaction.expenses()
     );
-    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(
       Transaction.total()
     );
   },
 
   clearTransactions() {
-    DOM.transactionsContainer.innerHTML = "";
+    DOM.transactionsContainer.innerHTML = '';
   },
 };
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value.replace(/\,\./g, "")) * 100;
+    value = Number(value.replace(/\,\./g, '')) * 100;
 
     return value;
   },
 
   formatDate(date) {
-    const splittedDate = date.split("-");
+    const splittedDate = date.split('-');
     return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
   },
 
   formatCurrency(value) {
-    const signal = Number(value) < 0 ? "-" : "";
+    const signal = Number(value) < 0 ? '-' : '';
 
-    value = String(value).replace(/\D/g, "");
+    value = String(value).replace(/\D/g, '');
 
     value = Number(value) / 100;
 
-    value = value.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL",
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
     });
 
     return signal + value;
@@ -133,9 +133,9 @@ const Utils = {
 };
 
 const Form = {
-  description: document.querySelector("input#description"),
-  amount: document.querySelector("input#amount"),
-  date: document.querySelector("input#date"),
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
 
   getValues() {
     return {
@@ -149,11 +149,11 @@ const Form = {
     const { description, amount, date } = Form.getValues();
 
     if (
-      description.trim() === "" ||
-      amount.trim() === "" ||
-      date.trim() === ""
+      description.trim() === '' ||
+      amount.trim() === '' ||
+      date.trim() === ''
     ) {
-      throw new Error("Por favor, preencha todos os campos");
+      throw new Error('Por favor, preencha todos os campos');
     }
   },
 
@@ -172,9 +172,9 @@ const Form = {
   },
 
   clearFields() {
-    Form.description.value = "";
-    Form.amount.value = "";
-    Form.date.value = "";
+    Form.description.value = '';
+    Form.amount.value = '';
+    Form.date.value = '';
   },
 
   submit(event) {
@@ -205,5 +205,11 @@ const App = {
     App.init();
   },
 };
+
+const toggleButton = document.getElementById('toggle-button');
+const body = document.querySelector('body')
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark')
+})
 
 App.init();
